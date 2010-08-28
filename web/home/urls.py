76317babc45f0e5282c1c67a010d9746c043d304
Arun Tejasvi Chaganty <arunchaganty@gmail.com>
2010-08-29 03:08:36 +0530

    Moved web interface away

--- web/home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('web.home.views',
-    (r'^register/$', 'register'),
-    (r'^logout/$', 'logout'),
-    (r'^login/$', 'login'),
-    (r'^$', 'home'),
-)
--- web/judge/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('web.judge.views',
-    (r'^manage/$', 'manage'),
-    (r'^standings/$', 'standings'),
-    (r'^results/$', 'results'),
-)
